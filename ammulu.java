97856d34277458f7daa54e659a1576cb116d4931
monica <moni@amulu.com>
2018-12-05 16:40:44 +0530

    new changes

---
+++ ammulu.java
@@ -0,0 +1,18 @@
+class Hello
+{
+   public static void main(String[]args)
+
+{
+    System.out.println("hi monica");
+
+
+
+}
+
+
+}
+
+
+
+
+https://www.quora.com/What-is-Jenkins-When-and-why-is-it-used
---
+++ employee.java
@@ -0,0 +1,13 @@
+class Employee
+{
+
+  public static void main(String args[])
+
+{
+   System.out.println("monica");
+
+
+}
+
+
+}
